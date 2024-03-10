45e263ccdc97efb27c3c8905397529864a8e230c
Alex Pirogov <alex-pirogov@qa.studio>
2024-03-10 19:08:54 +0300

    hanson metric

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,18 @@
 import esprima from 'esprima';
+// @ts-ignore
+import * as Styx from 'styx';
 
 export interface IMetric {
   name: string;
   info: string;
   scope: 'any' | 'module' | 'class' | 'function';
-  run: (program: esprima.Program) => IMetricResult;
+  run: (
+    program: esprima.Program,
+    programFlow?: typeof Styx.parse
+  ) => IMetricResult;
 }
 
 export interface IMetricResult {
-  value: number | number[];
+  value: any;
   description?: string;
 }
