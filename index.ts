780be82f69ef4abd81eca3f7188fef62aede049f
Andrey Kulevich <dronaldo12345@gmail.com>
2024-04-07 21:31:54 +0300

    added weighted methods per class

--- index.ts
+++ index.ts
@@ -6,38 +6,94 @@ import { Identifier, traverse } from '@babel/types';
 import AfferentCoupling from './src/metrics/AfferentCoupling';
 import EfferentCoupling from './src/metrics/EfferentCoupling';
 import Abstractness from './src/metrics/Abstractness';
+import WeightedMethodsPerClass from './src/metrics/WeightedMethodsPerClass';
+// @ts-ignore
+import * as Styx from 'styx';
 
-const projIndex = process.argv.indexOf('--proj');
+// const projIndex = process.argv.indexOf('--proj');
 
-if (projIndex === -1) {
-  console.error('Error: please, specify the path project JSON tree to analyze');
-  process.exit(1);
-}
+// if (projIndex === -1) {
+//   console.error('Error: please, specify the path project JSON tree to analyze');
+//   process.exit(1);
+// }
 
-const confIndex = process.argv.indexOf('--conf');
+// const confIndex = process.argv.indexOf('--conf');
 
-if (confIndex === -1) {
-  console.error('Error: please, specify the path to configuration JSON file');
-  process.exit(1);
-}
+// if (confIndex === -1) {
+//   console.error('Error: please, specify the path to configuration JSON file');
+//   process.exit(1);
+// }
 
-const project: IModule = JSON.parse(fs.readFileSync(process.argv[projIndex + 1]).toString());
+// const project: IModule = JSON.parse(fs.readFileSync(process.argv[projIndex + 1]).toString());
 
-console.log(new Abstractness().run(findPathInFileTree('tests', project) as IModule, 'tests/module1'));
+// console.log(new Abstractness().run(findPathInFileTree('tests', project) as IModule, 'tests/module1'));
 
 
 console.log('Starting...');
 
 const text = `
-let foo = 'bar';
-foo = foo + ' puk puk';
-const dammit = (chpok) => {
-  if (chpok.length > 5) {
-    chpok + ' dfjidfj';
-  } 
-  return chpok;
+class A {
+  prop1 = 1
+  
+  method1() {
+    const foo = 'bla';
+    if (confIndex === -1) {
+      console.error('Error: please, specify the path to configuration JSON file');
+      for(const i = 0; i < 20; i++) {
+        if (i % 2 == 0) return 2;
+        else {
+          for(const i = 0; i < 20; i++) {
+            if (i % 2 == 0) return 2;
+            else return 1;
+          }
+        }
+      }
+      process.exit(1);
+    } else if (gigi_gaga > 0) {
+      for(const i = 0; i < 20; i++) {
+        if (i % 2 == 0) return 2;
+        else return 1;
+      }
+    }
+  }
+
+  method2() {
+    const a = 3;
+    var c;
+    const b = 4;
+    if (a > b) {
+      c = a
+    } else {
+      c = b
+    }
+    console.log(c);
+  }
+
+  method3() {
+    var jsonData;
+
+    var ext = path.extname(cmd.argumentDatasource.filename).toLowerCase();
+    var rawData  = fs.readFileSync(cmd.argumentDatasource.filename).toString();
+    switch(ext) {
+        case ".config":
+            jsonData = xml.parseString(rawData);
+            break;
+        case ".xml":
+            jsonData = xml.parseString(rawData);
+            break;
+        case ".json":
+            jsonData = JSON.parse(cmd.argumentDatasource.filename);
+            break;
+        case ".js":
+            jsonData = require(cmd.argumentDatasource.filename);
+            break;
+        default:
+            var msg = colors.bgRed.white(cmd.argumentDatasource.filename + " not supported as data storage");
+            console.log(msg);
+    }
+    return jsonData;
+  }
 }
-console.log(dammit(foo));
 `;
 
 const program = parser.parse(text, {
@@ -47,7 +103,9 @@ const program = parser.parse(text, {
   ]
 });
 
-// const programFlow = Styx.parse(program.program);
+//const programFlow = Styx.parse(program.program);
+
+console.log(new WeightedMethodsPerClass().run(program));
 
 // console.log(new HansonMetric().run(program, programFlow));
 // console.log(new MaintainabilityIndex().run(program, programFlow));
---
+++ src/metrics/WeightedMethodsPerClass.ts
@@ -0,0 +1,51 @@
+import { IMetric } from "../types";
+import { ParseResult } from '@babel/parser';
+import { traverse, File } from '@babel/types';
+// @ts-ignore
+import * as Styx from 'styx';
+import McCabeCC from "./McCabeCC";
+
+export default class WeightedMethodsPerClass implements IMetric {
+  private _name = 'Weighted Methods Per Class';
+  private _info = 'Weighted Methods Per Class';
+  private _scope = 'class';
+
+  public get name() {
+    return this._name;
+  }
+
+  public get info() {
+    return this._info;
+  }
+
+  public get scope() {
+    return this._scope as any;
+  }
+
+  public run(program: ParseResult<File>) {
+    
+    let weightedMethodsPerClass = 0;
+
+    traverse(program, { 
+      enter(node) {
+        if (node.type === 'ClassBody') {
+          for (const item of node.body) {
+            //@ts-ignore
+            if (item.type === 'MethodDefinition') {
+              console.log('bla');
+              //@ts-ignore
+              const programFlow = Styx.parse({
+                type: 'Program',
+                //@ts-ignore
+                body: item.value.body.body,
+                sourceType: 'script'
+              });
+              weightedMethodsPerClass += new McCabeCC().run(programFlow).value;
+            }
+          }
+        }
+    }});
+    
+    return { value: weightedMethodsPerClass };
+  } 
+}
